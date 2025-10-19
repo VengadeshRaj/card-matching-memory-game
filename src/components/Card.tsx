affd7b5be58550cc3d3bb8ba2a8c6f8ee037a0f4
vengadeshraj <vengadeshraj405@gmail.com>
2025-10-20 01:25:18 +0530

    style update

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,7 +13,7 @@ const Card = (props: CardProps) => {
   if (isFrozen) {
     return (
       <div className="relative inline-flex items-center justify-center  text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 w-24 h-24">
-        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 rounded-md">
+        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 rounded-md font-bold text-4xl">
           {cardNumber}
         </span>
       </div>
@@ -23,7 +23,7 @@ const Card = (props: CardProps) => {
     <div onClick={() => onClick(index, cardNumber)} className="cursor-pointer">
       {isRevealed || isFrozen ? (
         <div className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800 w-24 h-24">
-          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 rounded-md">
+          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 rounded-md font-bold text-4xl">
             {cardNumber}
           </span>
         </div>
