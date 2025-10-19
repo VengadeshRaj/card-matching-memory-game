441bc57255d2aca4d5e7410a3093fa66f3ff3f23
vengadeshraj <vengadeshraj405@gmail.com>
2025-10-20 01:16:52 +0530

    image updated in README

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 The number of cards is displayed based on the player’s selected size. When the player clicks on a card, it reveals a number. A matching pair of that number exists among the remaining cards. If the player selects another card with the same number, both cards remain revealed. However, if the player selects a card with a different number, both cards are flipped back and remain hidden.
 
+![Memory Game](./game-images/game-picture.png)
+
 ## Prerequisites:
 
 - Node.js installed.
--- src/MemoryGame.tsx
+++ src/MemoryGame.tsx
@@ -108,7 +108,7 @@ const MemoryGame = () => {
     SIZE.map((s) => <option value={s} className="">{s}</option>);
 
   return (
-    <div className="flex flex-col items-center gap-4 bg-white dark:bg-gray-900 min-h-screen">
+    <div className="flex flex-col items-center gap-4 bg-white dark:bg-gray-900 min-h-screen p-8">
       <div className="flex">
         <h3 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
           <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
