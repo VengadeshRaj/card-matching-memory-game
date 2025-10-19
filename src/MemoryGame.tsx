c8543858130fc0863df89b043e0563c0a1e7c751
vengadeshraj <vengadeshraj405@gmail.com>
2025-10-19 11:56:47 +0530

    App base setup initiated

---
+++ src/MemoryGame.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MemoryGame = () => {
+  return (
+    <div>
+      <h1> Hello from memory game</h1>
+      <button className="text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">
+        Reset Game
+      </button>
+    </div>
+  );
+};
+
+export default MemoryGame;
